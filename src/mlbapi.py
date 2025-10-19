22de47c9d754f5369fa89f5b6a1b3548a98122ae
Yash Patel <ytpatel3@gmail.com>
2025-10-18 20:20:24 -0400

    switch to polars

--- src/mlbapi.py
+++ src/mlbapi.py
@@ -4,18 +4,20 @@ HW3
 mlbapi.py: API backend
 """
 
-import pandas as pd
+import polars as pl
 
 class MLBAPI:
 
     def __init__(self, path):
-        self.df = pd.read_csv(path)
+        self.df = pl.read_csv(path)
         self._clean_data()
 
     def _clean_data(self):
         """standardize column names and replace nulls with 0 (0th percentile)"""
-        self.df.columns = [col.lower().strip().replace(' ','_') for col in self.df.columns]
-        self.df.fillna(0, inplace=True)
+        
+        self.df = self.df.rename({col: col.lower().strip().replace(' ','_') for col in self.df.columns})
+        self.df = self.df.fill_null(0)
+
 
 
 if __name__ == '__main__':
